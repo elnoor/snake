721b430037bb32d028f49671f24817fb0701a08d
Elnur Mammadli <elnoormobile@gmail.com>
2020-04-26 17:24:13 -0600

    fauna query updated

--- functions/create-top-scorer.js
+++ functions/create-top-scorer.js
@@ -1,30 +1,31 @@
-import faunadb from 'faunadb';
+import faunadb from "faunadb";
 
-const q = faunadb.query
+const q = faunadb.query;
 const client = new faunadb.Client({
-  secret: process.env.FAUNADB_SERVER_SECRET
-})
+  secret: process.env.FAUNADB_SERVER_SECRET,
+});
 
 /* export our lambda function as named "handler" export */
 exports.handler = (event, context, callback) => {
   /* parse the string body into a useable JS object */
-  debugger;
-  const data = JSON.parse(event.body)
-  console.log("Function `todo-create` invoked", data)
+  const newScore = JSON.parse(event.body);
+  console.log("Function `create-top-scorer` invoked with data: ", newScore);
 
   /* construct the fauna query */
-  return client.query(q.Create(q.Ref("TopScorers"), {data}))
-  .then((response) => {
-    console.log("success", response)
-    return callback(null, {
-      statusCode: 200,
-      body: JSON.stringify(response)
+  return client
+    .query(q.Create(q.Collection("TopScorers"), { data: newScore }))
+    .then((response) => {
+      console.log("success", response);
+      return callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(response),
+      });
     })
-  }).catch((error) => {
-    console.log("error", error)
-    return callback(null, {
-      statusCode: 400,
-      body: JSON.stringify(error)
-    })
-  })
-}
+    .catch((error) => {
+      console.log("error", error);
+      return callback(null, {
+        statusCode: 400,
+        body: JSON.stringify(error),
+      });
+    });
+};
